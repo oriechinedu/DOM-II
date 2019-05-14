848d5007d2131abee9c5ce540c76993f4e5c7a8f
Orie Chinedu <oriechinedu@gmail.com>
2019-05-14 13:46:02 +0100

    Implement wheel event

--- js/index.js
+++ js/index.js
@@ -10,6 +10,27 @@ images.forEach(image => {
     }, 500);
   })
 
+})
+
+document.addEventListener('keydown', (event) => {
+  alert(`Oops! did you press the ${event.key} key ?`)
+})
+
+const paragraphs = document.querySelectorAll('p');
+
+paragraphs.forEach(p => {
+  p.addEventListener('wheel', (event) => {
+    let fontSize = parseFloat(getComputedStyle(p, null).getPropertyValue('font-size'));
+    let newFontSize = fontSize + event.deltaY;
+    if (newFontSize > 24 || newFontSize < 10) {
+      newFontSize = 16;
+    }
+    p.style.fontSize = `${newFontSize}px`;
+    setTimeout(() => {
+      p.style.fontSize = `16px`
+    }, 2000)
+  } )
   
+
 })
 
