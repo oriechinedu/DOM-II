0c6a99a761e943e15e0d91908b5e1173de98cb35
Orie Chinedu <oriechinedu@gmail.com>
2019-05-14 12:33:59 +0100

    Implement mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+const images = document.querySelectorAll('img')
+
+images.forEach(image => {
+  image.addEventListener('mouseover', () => {
+    image.classList.add('hovered');
+    setTimeout(() => {
+      image.classList.remove('hovered');
+    }, 500);
+  })
+
+  
+})
+
